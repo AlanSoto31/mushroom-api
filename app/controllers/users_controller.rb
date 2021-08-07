4232d02ee4a6debf31e0ecad417faafdcf1df27b
AlanSoto31 <soto.alan@hotmail.com>
2021-08-07 18:26:44 -0500

    Add create method to User controller using bcrypt gem

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {data: user}
+    else
+      render json: {data: user.errors}
+    end
+  end 
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+  
+end
+

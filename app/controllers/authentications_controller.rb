1716e2a4c498c179aaf5ff00aed7b0b1ccfb5940
AlanSoto31 <soto.alan@hotmail.com>
2021-08-07 20:28:46 -0500

    Asign tokens to users

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,7 +4,12 @@ class AuthenticationsController < ApplicationController
 
         if user 
             if user.authenticate(params[:password])
-                render json: {message: "Correct password"}, status: :ok
+                secret_key = Rails.application.secrets.secret_key_base[0]
+                token = JWT.encode({
+                    user_id: user.id,
+                    user_name: user.name,
+                }, secret_key)
+                render json: {token: token}, status: :ok
             else
                 render json: {error: "Wrong password"}, status: :unauthorized
             end

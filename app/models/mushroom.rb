6609af71f107db6ca5024e6bf724f83d06c69748
AlanSoto31 <soto.alan@hotmail.com>
2021-09-06 16:51:51 -0500

    Test Mushroom model

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -1,5 +1,14 @@
 class Mushroom < ApplicationRecord
     has_many :favorites, dependent: :delete_all
     has_many :users, through: :favorites
-    validates_presence_of :name, :img_url, :description, :scientific_name, :wiki_url, :edibility, :dimensions, :location
+
+    validates :name, presence: true, length: { in: 2..15 }
+    validates :img_url, presence: true, length: { minimum: 20 }
+    validates :description, presence: true, length: {in: 10..300}
+    validates :scientific_name, presence: true, length: { in: 2..20 }
+    validates :wiki_url, presence: true, length: { minimum: 20 }
+    validates :edibility, presence: true, length: { minimum: 2 }
+    validates :dimensions, presence: true, length: { minimum: 2 }
+    validates :location, presence: true, length: { minimum: 2 }
+
 end
---
+++ spec/models/mushroom_spec.rb
@@ -0,0 +1,120 @@
+require 'rails_helper'
+
+RSpec.describe Mushroom, type: :model do
+  subject { Mushroom.new(
+    name: 'Oyster', 
+    img_url: 'https://en.wikipedia.org/wiki/Pleurotus_ostreatus', 
+    description: 'description', 
+    scientific_name: 'Oyster operandis', 
+    wiki_url:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Neutari_%28oyster_mushroom%29.jpg/150px-Neutari_%28oyster_mushroom%29.jpg', 
+    edibility: 'taste', 
+    dimensions: '13 x 5 cm', 
+    location: 'Worldwide'
+    ) }
+
+  context 'Validations' do
+    
+    it 'name must be present' do
+      expect(subject).to be_valid
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'name must contain between 2 and 15 characters' do
+      expect(subject).to be_valid
+      subject.name = 'a'
+      expect(subject).to_not be_valid
+      subject.name = 'aaaaaaaaaaaaaaaa'
+      expect(subject).to_not be_valid
+    end
+
+    it 'img_url must be present' do
+      expect(subject).to be_valid
+      subject.img_url = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'img_url must contain minimum 20 characters' do
+      expect(subject).to be_valid
+      subject.img_url = 'www.n.com'
+      expect(subject).to_not be_valid
+    end
+
+    it 'description must be present' do
+      expect(subject).to be_valid
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'description must contain between 10 and 300 characters' do
+      expect(subject).to be_valid
+      subject.description = 'good'
+      expect(subject).to_not be_valid
+      subject.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      expect(subject).to_not be_valid
+    end
+
+    it 'scientific_name must be present' do
+      expect(subject).to be_valid
+      subject.scientific_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'scientific_name must contain between 2 and 20 characters' do
+      expect(subject).to be_valid
+      subject.scientific_name = 'a'
+      expect(subject).to_not be_valid
+      subject.scientific_name = 'aaaaaaaaaaaaaaaaaaaaaaaa'
+      expect(subject).to_not be_valid
+    end
+
+    it 'wiki_url must be present' do
+      expect(subject).to be_valid
+      subject.wiki_url = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'wiki_url must contain minimum 20 characters' do
+      expect(subject).to be_valid
+      subject.wiki_url = 'www.n.com'
+      expect(subject).to_not be_valid
+    end
+
+    it 'edibility must be present' do
+      expect(subject).to be_valid
+      subject.edibility = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'edibility must contain minimum 2 characters' do
+      expect(subject).to be_valid
+      subject.edibility = 'a'
+      expect(subject).to_not be_valid
+    end
+
+    it 'dimensions must be present' do
+      expect(subject).to be_valid
+      subject.dimensions = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'dimensions must contain minimum 2 characters' do
+      expect(subject).to be_valid
+      subject.dimensions = 'a'
+      expect(subject).to_not be_valid
+    end
+
+    it 'location must be present' do
+      expect(subject).to be_valid
+      subject.location = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'location must contain minimum 2 characters' do
+      expect(subject).to be_valid
+      subject.location = 'a'
+      expect(subject).to_not be_valid
+    end
+  
+  end
+end

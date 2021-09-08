a82107905ca501894d447d4cb528ca82fa8e89ec
AlanSoto31 <soto.alan@hotmail.com>
2021-09-08 09:38:49 -0500

    Add Delete and Update actions to Mushroom controller

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -1,6 +1,6 @@
 class MushroomsController < ApplicationController
-
-    before_action :authenticate, only: [:create]
+    before_action :authenticate, only: [:create, :update, :destroy]
+    before_action :admin, only: [:create, :update, :destroy]
 
     def index
         mushrooms = Mushroom.all
@@ -8,23 +8,46 @@ class MushroomsController < ApplicationController
     end
 
     def create
-        if @user.admin
-            mushroom = Mushroom.new(mushroom_params)
-            if mushroom.save
-                render json: {data: mushroom}
-            else
-                render json: {error: mushroom.errors}, status: :unprocessable_entity
-            end
-        
+        mushroom = Mushroom.new(mushroom_params)
+        if mushroom.save
+            render json: {data: mushroom}, status: :created
         else
-            render status: :unauthorized
-        end  
+            render json: { error: mushroom.errors }, status: :unprocessable_entity
+        end
     end
+
+    def update
+        mushroom = find_mush(params[:id])
+        if mushroom.update(mushroom_params)
+            render json: {data: mushroom}, status: :ok
+        else
+            render json:{ error: mushroom.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        mushroom = find_mush(params[:id])
+        if mushroom.destroy
+            head(:ok)
+        else
+            head(:unprocessable_entity)
+        end
+    end
+    
 
     private
 
     def mushroom_params
         params.require(:mushroom).permit(:name, :img_url, :description, :scientific_name, :wiki_url, :edibility, :dimensions, :location)
+    end
+
+    def admin
+        #@user.admin ? @user.admin : (render status: :unauthorized )
+        render status: :unauthorized unless @user.admin
+    end
+
+    def find_mush(id)
+        Mushroom.find(id) 
     end
     
 end
